265b30591f7b4d1b676e2fb07534de45d6954373
Patryk Sąsiadek <patrykskontakt@gmail.com>
2024-02-14 04:16:14 +0100

    API_URL, although working on it

--- rest-countries-api/src/app/page.js
+++ rest-countries-api/src/app/page.js
@@ -3,12 +3,12 @@ import { Suspense } from 'react';
 import { FilterSection } from '@/components/filter-section/FilterSection';
 import { Countries } from '@/components/countries/Countries';
 
-export default async function FrontPage() {
+export default async function FrontPage({ searchParams }) {
 	return (
 		<main className="py-9 px-4 sm:px-8 md:px-12">
 			<FilterSection />
 			<Suspense fallback={<p>Loading...</p>}>
-				<Countries />
+				<Countries searchParams={searchParams} />
 			</Suspense>
 		</main>
 	);
--- rest-countries-api/src/components/countries/Countries.jsx
+++ rest-countries-api/src/components/countries/Countries.jsx
@@ -1,15 +1,37 @@
 import { Wrapper } from '@/ui/Wrapper';
 import Image from 'next/image';
 
-export async function Countries() {
-	const res = await fetch(`https://restcountries.com/v3.1/all?fields=flags,name,population,region,capital`);
+const defaultQuery = 'all?fields=flags,name,population,region,capital';
+
+const getQuery = searchParams => {
+	const [firstEntry] = Object.entries(searchParams);
+
+	if (!firstEntry) {
+		return defaultQuery;
+	}
+
+	const [key, value] = firstEntry;
+
+	return `region/${value}`;
+};
+
+export async function Countries({ searchParams }) {
+	const [firstEntry] = Object.entries(searchParams);
+
+	const API__URL = firstEntry ? `region/${firstEntry[1]}` : defaultQuery;
+	const API_URL = getQuery(searchParams);
+	// define which way is better, get data validating with zod, first maybe check if its even a viable
+	// solution and if not should just opt into client component, as the problems may arise
+	// while trying to search by both region and searchbar
+
+	const res = await fetch(`https://restcountries.com/v3.1/${API_URL}`);
 	const data = await res.json();
 
 	return (
 		<Wrapper>
 			<ul>
 				{data.map(({ flags: { png: src, alt }, name: { official: name }, population, region, capital }) => (
-					<li key={name}>
+					<li key={name} className="relative">
 						<Image src={src} alt={alt} fill />
 						<div>
 							<h2>{name}</h2>
