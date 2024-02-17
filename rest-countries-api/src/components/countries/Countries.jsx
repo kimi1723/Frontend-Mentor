a8d6df01cbe9db9f5a365dadd951c0a1c3ae5d9a
Patryk Sąsiadek <patrykskontakt@gmail.com>
2024-02-17 23:56:03 +0100

    z index fix when hovering an card it overlaped the filter list:

--- rest-countries-api/src/components/countries/Countries.jsx
+++ rest-countries-api/src/components/countries/Countries.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 
 import { Wrapper } from '@/ui/Wrapper';
+import Link from 'next/link';
 
 export async function Countries({ searchParams }) {
 	const filter = searchParams.filter?.toLowerCase();
@@ -12,36 +13,39 @@ export async function Countries({ searchParams }) {
 		`https://restcountries.com/v3.1/${API_FILTER_URL}?fields=flags,name,population,region,capital`,
 	);
 	const data = await res.json();
-
 	return (
 		<Wrapper>
 			<ul className="flex items-center justify-center flex-wrap gap-14 lg:gap-20 my-7 py-5 w-full">
 				{data
 					.filter(country => country.name.common.toLowerCase().includes(searchQuery))
 					.map(({ flags: { png: src, alt }, name: { common: name }, population, region, capital }) => (
-						<li key={name} className="w-72 bg-white dark:bg-dark-blue shadow-lg rounded-md">
-							<Image src={src} alt={alt} height={192} width={288} className="rounded-t-md shadow-sm h-40" />
+						<li
+							key={name}
+							className="w-72 bg-white dark:bg-dark-blue shadow-lg rounded-md ease-in-out duration-300 hover:shadow-2xl hover:scale-105 z-0">
+							<Link href="/" className="hover:focus:shadow-2xl hover:focus:scale-105">
+								<Image src={src} alt={alt} height={160} width={288} className="rounded-t-md shadow-sm h-40" />
 
-							<div className="mt-0.5 px-7 pt-6 pb-12 rounded-b-md">
-								<h2 className="font-bold text-xl mb-3">{name}</h2>
+								<div className="mt-0.5 px-7 pt-6 pb-12 rounded-b-md">
+									<h2 className="font-bold text-xl mb-3 cursor-default">{name}</h2>
 
-								<dl className="flex flex-col gap-y-1">
-									<div>
-										<dt className="font-semibold inline">Population: </dt>
-										<dd className="inline">{population.toLocaleString()}</dd>
-									</div>
+									<dl className="flex flex-col gap-y-1">
+										<div>
+											<dt className="font-semibold inline cursor-default">Population: </dt>
+											<dd className="inline cursor-default">{population.toLocaleString()}</dd>
+										</div>
 
-									<div>
-										<dt className="font-semibold inline">Region: </dt>
-										<dd className="inline">{region}</dd>
-									</div>
+										<div>
+											<dt className="font-semibold inline cursor-default">Region: </dt>
+											<dd className="inline cursor-default">{region}</dd>
+										</div>
 
-									<div>
-										<dt className="font-semibold inline">Capital: </dt>
-										<dd className="inline">{capital}</dd>
-									</div>
-								</dl>
-							</div>
+										<div>
+											<dt className="font-semibold inline cursor-default">Capital: </dt>
+											<dd className="inline cursor-default">{capital}</dd>
+										</div>
+									</dl>
+								</div>
+							</Link>
 						</li>
 					))}
 			</ul>
--- rest-countries-api/src/components/filter-section/filter/FilterList.jsx
+++ rest-countries-api/src/components/filter-section/filter/FilterList.jsx
@@ -6,7 +6,7 @@ const lastIndex = options.length - 2;
 
 export function FilterList({ ariaControlId, onClick, activeRegion = 'all' }) {
 	return (
-		<ul className="bg-vd-blue absolute top-full w-full mt-1 shadow-lg rounded-lg" id={ariaControlId}>
+		<ul className="bg-vd-blue absolute top-full w-full mt-1 shadow-lg rounded-lg z-10" id={ariaControlId}>
 			{options
 				.filter(option => option.toLowerCase() !== activeRegion)
 				.map((region, index) => {
