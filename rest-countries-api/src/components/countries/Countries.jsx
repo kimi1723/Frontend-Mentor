ea5b904b268497c9f152eb1c605280b462ce8897
Patryk Sąsiadek <patrykskontakt@gmail.com>
2024-02-19 01:08:47 +0100

    text can be selectable in country. separated it into single component

--- rest-countries-api/src/components/countries/Countries.jsx
+++ rest-countries-api/src/components/countries/Countries.jsx
@@ -1,8 +1,6 @@
-import Image from 'next/image';
-
 import { Wrapper } from '@/ui/Wrapper';
-import Link from 'next/link';
-import { HeadingTwo } from '@/ui/h2';
+
+import { SingleCountry } from './SingleCountry';
 
 export async function Countries({ searchParams }) {
 	const filter = searchParams.filter?.toLowerCase();
@@ -20,36 +18,8 @@ export async function Countries({ searchParams }) {
 			<ul className="flex items-center justify-center flex-wrap gap-14 lg:gap-20 my-7 py-5 w-full">
 				{data
 					.filter(country => country.name.common.toLowerCase().includes(searchQuery))
-					.map(({ flags: { png: src, alt }, name: { common: name }, population, region, capital }) => (
-						<li
-							key={name}
-							className="w-72 bg-white dark:bg-dark-blue shadow-lg rounded-md ease-in-out duration-300 hover:shadow-2xl hover:scale-105 z-0">
-							<Link href="/a" className="hover:focus:shadow-2xl hover:focus:scale-105">
-								<Image src={src} alt={alt} height={160} width={288} className="rounded-t-md shadow-sm h-40" />
-								{/* try to work on the focus as it doesnt work as intended(hoevr does), make the text copyable */}
-								<div className="mt-0.5 px-7 pt-6 pb-12 rounded-b-md">
-									<HeadingTwo text={name} className="font-bold text-xl mb-3 cursor-default select-text" />
-									{/* <h2 className="font-bold text-xl mb-3 cursor-default">{name}</h2> */}
-
-									<dl className="flex flex-col gap-y-1">
-										<div>
-											<dt className="font-semibold inline cursor-default">Population: </dt>
-											<dd className="inline cursor-default">{population.toLocaleString()}</dd>
-										</div>
-
-										<div>
-											<dt className="font-semibold inline cursor-default">Region: </dt>
-											<dd className="inline cursor-default">{region}</dd>
-										</div>
-
-										<div>
-											<dt className="font-semibold inline cursor-default">Capital: </dt>
-											<dd className="inline cursor-default">{capital}</dd>
-										</div>
-									</dl>
-								</div>
-							</Link>
-						</li>
+					.map(country => (
+						<SingleCountry key={country.name.common} country={country} />
 					))}
 			</ul>
 		</Wrapper>
---
+++ rest-countries-api/src/components/countries/SingleCountry.jsx
@@ -0,0 +1,54 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+export function SingleCountry({
+	country: {
+		flags: { png: src, alt },
+		name: { common: name },
+		population,
+		region,
+		capital,
+	},
+}) {
+	const handleTextClick = e => {
+		const textTags = ['h2', 'dt', 'dd'];
+		const tagName = e.target.tagName.toLowerCase();
+
+		if (textTags.includes(tagName)) {
+			e.preventDefault();
+		}
+	};
+
+	return (
+		<li
+			key={name}
+			className="w-72 bg-white dark:bg-dark-blue shadow-lg rounded-md ease-in-out duration-300 hover:shadow-2xl hover:scale-105 z-0">
+			<Link href="/a" className="hover:focus:shadow-2xl hover:focus:scale-105">
+				<Image src={src} alt={alt} height={160} width={288} className="rounded-t-md shadow-sm h-40" />
+				{/* try to work on the focus as it doesnt work as intended(hoevr does), make the text copyable */}
+				<div className="mt-0.5 px-7 pt-6 pb-12 rounded-b-md" onClick={handleTextClick}>
+					<h2 className="font-bold inline-block text-xl mb-3 cursor-text select-text">{name}</h2>
+
+					<dl className="flex flex-col gap-y-1">
+						<div>
+							<dt className="font-semibold inline cursor-text  select-text">Population: </dt>
+							<dd className="inline cursor-text  select-text">{population.toLocaleString()}</dd>
+						</div>
+
+						<div>
+							<dt className="font-semibold inline cursor-text  select-text">Region: </dt>
+							<dd className="inline cursor-text  select-text">{region}</dd>
+						</div>
+
+						<div>
+							<dt className="font-semibold inline cursor-text  select-text">Capital: </dt>
+							<dd className="inline cursor-text  select-text">{capital}</dd>
+						</div>
+					</dl>
+				</div>
+			</Link>
+		</li>
+	);
+}
