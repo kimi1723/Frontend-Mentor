fbacc016d3e93f0c5578942d60f82aa109141201
Patryk Sąsiadek <patrykskontakt@gmail.com>
2024-02-23 20:12:31 +0100

    border fixes

--- rest-countries-api/src/components/country-details/Borders.jsx
+++ rest-countries-api/src/components/country-details/Borders.jsx
@@ -1,20 +1,21 @@
 export function Borders({ borders }) {
 	return (
-		<>
-			<h3 className="mt-10 mb-4 font-semibold text-lg duration-300 ease-in-out">Border countries:</h3>
-			<ul className="flex flex-wrap gap-3">
-				{borders ? (
-					borders.map(border => (
+		<div className="mt-10 flex flex-col md:flex-row md:gap-x-5">
+			<h3 className="mb-4 font-semibold text-lg duration-300 ease-in-out md:mb-0">Border countries:</h3>
+
+			{borders ? (
+				<ul className="flex flex-wrap gap-3">
+					{borders.map(border => (
 						<li
 							key={border}
 							className="inline py-2 px-6 text-sm bg-white dark:bg-dark-blue shadow-lg flex-1 text-center duration-300 ease-in-out">
 							{border}
 						</li>
-					))
-				) : (
-					<p className="duration-300 ease-in-out">Country has no border countries</p>
-				)}
-			</ul>
-		</>
+					))}
+				</ul>
+			) : (
+				<p className="duration-300 ease-in-out">Country has no border countries</p>
+			)}
+		</div>
 	);
 }
